7194fc6056ff535af56a311a949db6fd5bdf8aa8
Thomas Francis Jr <franc@tcsaf.com>
2019-02-17 18:49:24 -0500

    Added code to add all lists to the ourGroceriesList type for the list slot
    Lots of fixes
    
    This hard-codes the location of config.ini, which probably isn't
    necessary.  Will test it without in a future commit.

--- action-addToList.py
+++ action-addToList.py
@@ -2,10 +2,13 @@
 
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
+from io import open
+from subprocess import Popen, PIPE, STDOUT
 import our_groceries_client
 import ConfigParser
+import json
 
-CONFIG_INI = "config.ini"
+CONFIG_INI = "/usr/share/snips/assistant/snippets/franc.Our_Groceries/config.ini"
 CONFIG_ENCODING = "utf-8"
 
 class SnipsConfigParser(ConfigParser.SafeConfigParser):
@@ -14,12 +17,25 @@ class SnipsConfigParser(ConfigParser.SafeConfigParser):
 
 def readConfigurationFile(fileName):
     try:
-        with io.open(fileName, encoding=CONFIG_ENCODING) as f:
+        with open(fileName, encoding=CONFIG_ENCODING) as f:
             confParser = SnipsConfigParser()
             confParser.readfp(f)
             return confParser.to_dict()
     except (IOError, ConfigParser.Error) as e:
         return dict()
+
+def getListsPayload(hermes):
+    config = readConfigurationFile(CONFIG_INI)
+    client = our_groceries_client.OurGroceriesClient()
+    client.authenticate(config['secret']['username'], config['secret']['password'], config['secret']['defaultlist'])
+    listNames = []
+    for listInfo in client._lists:
+        listNames.append(listInfo['name'])
+
+    operation = [ "add", { "ourGroceriesList" : listNames }];
+    operations = [ operation ]
+    payload = { 'operations': operations }
+    return json.dumps(payload)
 
 def addToList(hermes, intentMessage):
     what = intentMessage.slots.what.first().rawValue
@@ -27,12 +43,15 @@ def addToList(hermes, intentMessage):
     quantity = intentMessage.slots.quantity.first().value()
     config = readConfigurationFile(CONFIG_INI)
     client = our_groceries_client.OurGroceriesClient()
-    client.authenticate(config['secret']['username'], config['secret']['password'], config['secret']['defaultList'])
+    client.authenticate(config['secret']['username'], config['secret']['password'], config['secret']['defaultlist'])
     client.add_to_list(what, int(float(quantity)), list)
     sentence = 'Added ' + quanity + " " + what + "to " + list
     hermes.publish_end_session(intent_message.session_id, sentence)
 
 if __name__=="__main__":
     with Hermes("localhost:1883") as h:
+        payload = getListsPayload(h)
+        p = Popen(['/usr/bin/mosquitto_pub', '-t', 'hermes/injection/perform', '-m', payload], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        output = p.communicate()[0]
         h.subscribe_intent("franc:addToList",addToList).start()
 
