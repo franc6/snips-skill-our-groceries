ff723842e204d17da7e325a6eda24362298c7449
Thomas Francis Jr <franc@tcsaf.com>
2019-02-20 13:13:23 -0500

    Changed scope of HiddenPrints usage
    Also added import for sys

--- action-ourgroceriesIntentsParser.py
+++ action-ourgroceriesIntentsParser.py
@@ -2,6 +2,7 @@
 
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
+import sys
 from io import open
 from subprocess import Popen, PIPE, STDOUT
 import our_groceries_client
@@ -62,80 +63,82 @@ def getUpdatePayload(hermes):
     return json.dumps(payload)
 
 def addToList(hermes, intentMessage):
-    quantity = 1
-    what = None
-    whichList = None
-    for (slot_value, slot) in intentMessage.slots.items():
-        if (slot_value == 'what'):
-            what = slot[0].raw_value
-        elif (slot_value == 'list'):
-            whichList = slot[0].slot_value.value.value
-        elif (slot_value == 'quantity'):
-            quantity = int(float(slot[0].slot_value.value.value))
-    if (what is None):
-        return
-    # Set whichList to defaultlist if it's None or matches 'our groceries'
-    # The API would use the same list if we passed None, but the code below
-    # would fail when giving the response.
-    if (whichList is None) or (whichList == 'our groceries'):
-        whichList = config['secret']['defaultlist']
+    with HiddenPrints():
+        quantity = 1
+        what = None
+        whichList = None
+        for (slot_value, slot) in intentMessage.slots.items():
+            if (slot_value == 'what'):
+                what = slot[0].raw_value
+            elif (slot_value == 'list'):
+                whichList = slot[0].slot_value.value.value
+            elif (slot_value == 'quantity'):
+                quantity = int(float(slot[0].slot_value.value.value))
+        if (what is None):
+            return
+        # Set whichList to defaultlist if it's None or matches 'our groceries'
+        # The API would use the same list if we passed None, but the code below
+        # would fail when giving the response.
+        if (whichList is None) or (whichList == 'our groceries'):
+            whichList = config['secret']['defaultlist']
 
-    client = our_groceries_client.OurGroceriesClient()
-    client.authenticate(config['secret']['username'], config['secret']['password'], config['secret']['defaultlist'])
-    client.add_to_list(what, quantity, whichList)
+        client = our_groceries_client.OurGroceriesClient()
+        client.authenticate(config['secret']['username'], config['secret']['password'], config['secret']['defaultlist'])
+        client.add_to_list(what, quantity, whichList)
 
-    # Respond that we added it to the list
-    sentence = "Added {q} {w} to the {l} list.".format(q=quantity, w=what, l=whichList)
-    hermes.publish_end_session(intentMessage.session_id, sentence)
+        # Respond that we added it to the list
+        sentence = "Added {q} {w} to the {l} list.".format(q=quantity, w=what, l=whichList)
+        hermes.publish_end_session(intentMessage.session_id, sentence)
 
 def checkList(hermes, intentMessage):
-    quantity = '1'
-    what = None
-    whichList = None
-    sentence = None
-    for (slot_value, slot) in intentMessage.slots.items():
-        if (slot_value == 'what'):
-            what = slot[0].raw_value
-        elif (slot_value == 'list'):
-            whichList = slot[0].slot_value.value.value
-    if (what is None):
-        return
-    # Set whichList to defaultlist if it's None or matches 'our groceries'
-    # The API would use the same list if we passed None, but the code below
-    # would fail when giving the response.
-    if (whichList is None) or (whichList == 'our groceries'):
-        whichList = config['secret']['defaultlist']
+    with HiddenPrints():
+        quantity = '1'
+        what = None
+        whichList = None
+        sentence = None
+        for (slot_value, slot) in intentMessage.slots.items():
+            if (slot_value == 'what'):
+                what = slot[0].raw_value
+            elif (slot_value == 'list'):
+                whichList = slot[0].slot_value.value.value
+        if (what is None):
+            return
+        # Set whichList to defaultlist if it's None or matches 'our groceries'
+        # The API would use the same list if we passed None, but the code below
+        # would fail when giving the response.
+        if (whichList is None) or (whichList == 'our groceries'):
+            whichList = config['secret']['defaultlist']
 
-    client = our_groceries_client.OurGroceriesClient()
-    client.authenticate(config['secret']['username'], config['secret']['password'], config['secret']['defaultlist'])
-    items = client._get_list_data(whichList)
-    for item in items:
-        crossedOff = item.get('crossedOff', False)
-        # Note our two primary regular expressions are commented out below
-        # and combined into regex.  The uncommented regex line below this
-        # is just building the expression all at once.
-        #regex1 = r"^" + re.escape(what) + r" *$"
-        #regex2 = r"^" + re.escape(what) + r"\ \((\d+)\)$"
-        #regex = r"(" + regex1 + r")|(" + regex2 + r")"
-        regex = r"(^" + re.escape(what) + r" *$)|(^" + re.escape(what) + r"\ \((\d+)\)$)"
-        res = re.search(regex, item['value'], re.IGNORECASE)
-        # Note the following two conditions are not combined because we want to
-        # break the loop even if the item is crossed off.  If it's crossed off,
-        # we don't need to keep looking for a match -- you can't have the same
-        # item on the list with different case.  Perhaps with different
-        # spelling, but we're not doing sounds like checking here. :(
-        if res is not None:
-            if not crossedOff:
-                quantity = res.group(3)
-                if quantity is None:
-                    quantity = "1"
-                sentence = "There {v} {q} {w} on the {l} list".format(v="are" if int(quantity) > 1 else "is", q=quantity, w=what, l=whichList)
-            break
-    if sentence is None:
-        sentence = "{w} is not on the {l} list.".format(w=what, l=whichList)
+        client = our_groceries_client.OurGroceriesClient()
+        client.authenticate(config['secret']['username'], config['secret']['password'], config['secret']['defaultlist'])
+        items = client._get_list_data(whichList)
+        for item in items:
+            crossedOff = item.get('crossedOff', False)
+            # Note our two primary regular expressions are commented out below
+            # and combined into regex.  The uncommented regex line below this
+            # is just building the expression all at once.
+            #regex1 = r"^" + re.escape(what) + r" *$"
+            #regex2 = r"^" + re.escape(what) + r"\ \((\d+)\)$"
+            #regex = r"(" + regex1 + r")|(" + regex2 + r")"
+            regex = r"(^" + re.escape(what) + r" *$)|(^" + re.escape(what) + r"\ \((\d+)\)$)"
+            res = re.search(regex, item['value'], re.IGNORECASE)
+            # Note the following two conditions are not combined because we want to
+            # break the loop even if the item is crossed off.  If it's crossed off,
+            # we don't need to keep looking for a match -- you can't have the same
+            # item on the list with different case.  Perhaps with different
+            # spelling, but we're not doing sounds like checking here. :(
+            if res is not None:
+                if not crossedOff:
+                    quantity = res.group(3)
+                    if quantity is None:
+                        quantity = "1"
+                    sentence = "There {v} {q} {w} on the {l} list".format(v="are" if int(quantity) > 1 else "is", q=quantity, w=what, l=whichList)
+                break
+        if sentence is None:
+            sentence = "{w} is not on the {l} list.".format(w=what, l=whichList)
 
-    # Respond that we added it to the list
-    hermes.publish_end_session(intentMessage.session_id, sentence)
+        # Respond that we added it to the list
+        hermes.publish_end_session(intentMessage.session_id, sentence)
 
 def updateLists(hermes):
     payload = getUpdatePayload(hermes) + '\n'
@@ -143,11 +146,10 @@ def updateLists(hermes):
     stdout = p.communicate(input=payload.encode('utf-8'))[0]
 
 def intentCallback(hermes, intentMessage):
-    with HiddenPrints():
-        if intentMessage.intent.intent_name == 'franc:addToList':
-            addToList(hermes, intentMessage)
-        elif intentMessage.intent.intent_name == 'franc:checkList':
-            checkList(hermes, intentMessage)
+    if intentMessage.intent.intent_name == 'franc:addToList':
+        addToList(hermes, intentMessage)
+    elif intentMessage.intent.intent_name == 'franc:checkList':
+        checkList(hermes, intentMessage)
 
 if __name__=="__main__":
     config = readConfigurationFile(CONFIG_INI)
